28f3f96f116421e40bf50ef492208903c9716434
Ashe Urban <ashe@goldentongue.com>
2022-07-15 09:15:38 -0700

    ADD namespaces and class for Bakery and Bread.

---
+++ Bakery.Tests/ModelTests/BakeryTests.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bakery;
+
+namespace Bakery.TestTools
+{
+  [TestClass]
+  public class BreadTests
+  {
+    [TestMethod]
+    public void //METHOD GOES HERE!
+    {
+      //Bread freshBread = new Bread();
+      //ASSER GOES HERE!
+    }
+  }
+}
---
+++ Bakery/Models/Bread.cs
@@ -0,0 +1,13 @@
+using System;
+// using System.Collections.Generic;
+// using System.Linq;
+
+namespace Bakery
+{
+  public class Bread
+  {
+    //Public auto-implemented properties will go here!
+
+    //Constructor will go here!
+  }
+}
